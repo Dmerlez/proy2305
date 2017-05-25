25cff7ee1ea6f7e00de7c8e849cc647455dcfe1c
cetecom <cetecom@LC08L1222>
2017-05-25 11:58:48 -0400

    proyecto 23/05

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+         <form method="post" action="modelo/prod_agregar.php"</form>
+         <div><label>Nombre: </label><input type="text" name="nombre"></div>
+         <div><label>Tipo Producto: </label><input type="text" name="tipoproducto"></div>
+         <div><label>Precio: </label><input type="text" name="precio"></div>
+         <input type="submit">
+         <br>
+         <a href="m/prod_agregar.php">Link agregar</a>   
+    </body>
+</html>
